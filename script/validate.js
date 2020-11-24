5fdbe4f92c5bdb811dbc069b7124abbc468a6e6e
Peeyush Pandey <peeyushpandey245@gmail.com>
2020-11-25 02:47:20 +0530

    Update validate.js

--- script/validate.js
+++ script/validate.js
@@ -6,12 +6,24 @@ function validate() {
 	var mobile = document.getElementById("mobile").value;
 	console.log(name);
 	if(name.length==0)
+	{
 		window.alert("Name can not be empty");
+		return false;
+	}
 	else if(email.length==0)
+	{
 		window.alert("email can not be empty");
+		return false;
+	}
 	else if(password.length==0)
+	{
 		window.alert("password can not be empty");
+		return false;
+	}
 	else if(mobile.length==0)
+	{
 		window.alert("mobile can not be empty");
+		return false;
+	}
 }
 

adda96bdae0e66e24ffe63e1b52a9d392a02f025
Peeyush Pandey <peeyushpandey245@gmail.com>
2020-11-24 18:14:05 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -57,19 +57,19 @@ function stackScore() {
 }
 
 function validate() {
-	var name = document.getElementByID("name").value();
+	var name = document.getElementByID("name").value;
 	if(name.length()==0)
 		window.alert("Name can not be empty");
 	
-	var email = document.getElementByID("email").value();
+	var email = document.getElementByID("email").value;
 	if(email.length()==0)
 		window.alert("Email can not be empty");
 	
-	var password = document.getElementByID("password").value();
+	var password = document.getElementByID("password").value;
 	if(password.length()==0)
 		window.alert("Password can not be empty");
 	
-	var mobile = document.getElementByID("mobile").value();
+	var mobile = document.getElementByID("mobile").value;
 	if(mobile.length()==0)
 		window.alert("Mobile can not be empty");
 }
